b24ef090bc120f54051bc561d9f59a51a579dc15
Alexey Litvinov <iamhoneyboy@gmail.com>
2014-01-06 21:48:52 +0400

    fixed server

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
 var scripts = [
     'js.libs/jquery.js',
     'js.libs/underscore.js',
@@ -7,15 +10,35 @@ var scripts = [
     'js/view.scheme.js'
 ];
 
-var html = '<!doctype html><html lang="en"><head><meta charset="UTF-8"><title>weiqi test page</title><link rel="stylesheet" href="css/scheme.css">';
-html += scripts.map(function (s) {
+var html = '<!doctype html><html lang="en"><head><meta charset="UTF-8" /><title>weiqi</title>'
++ scripts.map(function (s) {
     return '<script type="text/javascript" src="' + s + '"></script>';
-});
-hmtl += '</head><body><script type="text/javascript" src="client.js"></script></body></html>';
+}).join('') + '</head><body><script type="text/javascript" src="client.js"></script></body></html>';
+
+
 
 http.createServer(function (req, res) {
-    res.setEncoding('utf8');
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(html);
-    res.end();
-}).listen(8080);
+    var parsedUrl = url.parse(req.url);
+    var path = parsedUrl.pathname;
+
+    if (/^\/[a-z\/\.]+\.[a-z]+$/i.test(path)) {
+        fs.readFile('.' + path, function (err, data) {
+            if (err) {
+                console.log(err.toString());
+                res.statusCode = 404;
+            } else {
+                if (/\.js$/i.test(path)) {
+                    res.writeHead(200, {'Content-Type': 'application/javascript'});
+                }
+
+                res.write(data);
+            }
+
+            res.end();
+        });
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(html);
+        res.end();
+    }
+}).listen(80);
